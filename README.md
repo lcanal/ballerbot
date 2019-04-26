92c31827405ac631f419ae5b481a1e7309e90a44
Luis A Canales <lacanales@ucdavis.edu>
2019-04-26 16:03:22 -0700

    Adding npm packages

---
+++ README.md
@@ -0,0 +1,10 @@
+# BallerBot
+
+Gonna show you a good time.
+
+## Init
+Remember to initialize your bot:
+
+```
+curl -F "url=https://alias.now.sh/<your_api_token>"  https://api.telegram.org/bot<your_api_token>/setWebhook
+```
--- index.js
+++ index.js
@@ -6,10 +6,16 @@ require("dotenv").config();
 
 var app = express();
 app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
 let telegram_url = "https://api.telegram.org/bot" + process.env.TEL_API_TOKEN +"/sendMessage";
 
 app.get('*', (req, res) => {
-    res.status(200).send('<h1>Hello, world!</h1>')
+    res.status(200).send("<h1>BallerBot says hi</h1>")
 })
 
 module.exports = app;
