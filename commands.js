545c95323b7a72732b97dec38f83271f839908fb
Luis Canales <cluchomucho@gmail.com>
2019-04-26 21:27:15 -0700

    refactored commands to own file.

---
+++ commands.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const axios = require("axios");
+let telegram_url  = "https://api.telegram.org/bot" + process.env.TEL_API_TOKEN;
+require("dotenv").config();
+
+module.exports = [
+    {
+        name    : 'hi',
+        method  : hiMethod
+    },
+    {
+        name    : 'nbatopscorer',
+        method  : nbaTopScorer
+    },
+    {
+        name    : 'unknown',
+        method  : unknownCommand
+    }
+];
+
+async function sendMessage(textToSend,chatid,res) {
+    axios.post(telegram_url+"/sendMessage",{
+      chat_id : chatid,
+      text    : textToSend
+    })
+    .then(response => {
+      console.log('Message posted')
+      res.end('ok')
+    })
+    .catch(err => {
+      console.log('Error :', err)
+      res.end('Error :' + err)
+    })
+}
+
+async function hiMethod(chat_id,res){
+    sendMessage('How you doin?',chat_id,res);
+}
+
+async function nbaTopScorer(chat_id,res){
+    sendMessage('Still learning this one...',chat_id,res)
+}
+
+async function unknownCommand(chat_id,res){
+    sendMessage("I don't know what you mean",chat_id,res);
+}
--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 "use strict";
-var express = require("express");
-var bodyParser = require("body-parser");
-const axios = require("axios");
-require("dotenv").config();
-
-var app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const commands = require("./commands")
+let app = express();
 app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-let telegram_url  = "https://api.telegram.org/bot" + process.env.TEL_API_TOKEN;
-let botname       = "ballerbot"
 
 // Bot response routines
-app.post('*',function (req,res){
+app.post('*',async function (req,res){
   const { message } = req.body
   if (typeof(message) === 'undefined' || typeof(message.text) === 'undefined') {
     // In case a message is not present do nothing and return an empty response
@@ -23,30 +14,19 @@ app.post('*',function (req,res){
   }
 
   // Actual response routines
-  const { text, chat } = message
-  if (text.toLowerCase().indexOf('hi') >= 0 ){
-    sendMessage('How you doin?',chat.id,res);
+  const { text, chat } = message;
+  for (const command of commands) {
+    if (text.toLowerCase().indexOf(command.name) >= 0) {
+      await command.method(chat.id,res);
+      return 
+    }
   }
-  else{
-    sendMessage("I don't know what you mean",chat.id,res);
-  }
+  
+  //No Matches
+  const [ unknown ] = commands.slice(-1)
+  console.log(unknown);
+  await unknown.method(chat.id,res);
 });
-
-async function sendMessage(textToSend,chatid,res) {
-  axios.post(telegram_url+"/sendMessage",{
-    chat_id : chatid,
-    text    : textToSend
-  })
-  .then(response => {
-    console.log('Message posted')
-    res.end('ok')
-  })
-  .catch(err => {
-    console.log('Error :', err)
-    res.end('Error :' + err)
-  })
-}
-//
 
 app.get('*', (req, res) => {
     res.status(200).send("<h1>BallerBot says hi</h1>")
