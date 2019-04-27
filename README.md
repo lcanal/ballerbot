a1cc7e2c5e2c79d44b7927c2903561a379a3e1b1
Luis A Canales <lacanales@ucdavis.edu>
2019-04-26 17:21:08 -0700

    Added routine hi logic

--- README.md
+++ README.md
@@ -6,5 +6,27 @@ Telegram bot to get your sports fix.
 Remember to initialize your bot:
 
 ```
-curl -F "url=https://alias.now.sh/<your_api_token>"  https://api.telegram.org/bot<your_api_token>/setWebhook
+curl -F "url=https://alias.now.sh"  https://api.telegram.org/bot<your_api_token>/setWebhook
+```
+
+## Running Locally
+I use now.sh to invoke the bot. You can run it locally:
+
+```
+npm install -g now 
+```
+
+Then you can get a local dev env:
+```
+npm run dev
+```
+
+You should now have *http://localhost:3000* available as if it were on now.sh
+
+## Deploy
+Before you can deploy to your now.sh account you'll need to set the right now token:
+
+```
+now secrets add ballerbot_token "<your_api_token>" 
+npm run deploy
 ```
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+"use strict";
 var express = require("express");
 var bodyParser = require("body-parser");
 const axios = require("axios");
 require("dotenv").config();
-
 
 var app = express();
 app.use(bodyParser.json());
@@ -11,11 +11,45 @@ app.use(
     extended: true
   })
 );
+let telegram_url  = "https://api.telegram.org/bot" + process.env.TEL_API_TOKEN;
+let botname       = "ballerbot"
 
-let telegram_url = "https://api.telegram.org/bot" + process.env.TEL_API_TOKEN +"/sendMessage";
+// Bot response routines
+app.post('*',function (req,res){
+  const { message } = req.body
+  if (typeof(message) === 'undefined' || typeof(message.text) === 'undefined') {
+    // In case a message is not present do nothing and return an empty response
+    return res.end('ok')
+  }
+
+  // Actual response routines
+  const { text, chat } = message
+  if (text.toLowerCase().indexOf('hi') >= 0 ){
+    sendMessage('How you doin?',chat.id,res);
+  }
+  else{
+    sendMessage("I don't know what you mean",chat.id,res);
+  }
+});
+
+async function sendMessage(textToSend,chatid,res) {
+  axios.post(telegram_url+"/sendMessage",{
+    chat_id : chatid,
+    text    : textToSend
+  })
+  .then(response => {
+    console.log('Message posted')
+    res.end('ok')
+  })
+  .catch(err => {
+    console.log('Error :', err)
+    res.end('Error :' + err)
+  })
+}
+//
 
 app.get('*', (req, res) => {
     res.status(200).send("<h1>BallerBot says hi</h1>")
-})
+});
 
 module.exports = app;
